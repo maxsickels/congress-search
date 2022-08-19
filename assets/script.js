bfa902b4d15e68293a47521fb9694ce472294bc9
Ben Robuck <brobuck87@gmail.com>
2022-08-19 09:04:00 -0400

    Add functionality to save user query results

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,14 @@
-var userContainer = document.getElementById('users');
-var fetchButton = document.getElementById('fetch-button');
-
+var userContainer = document.getElementById("users");
+var fetchButton = document.getElementById("fetch-button");
 
 function redirectPage() {
-  var userQuery = document.getElementById('user-query').value;
   //Capture User Input
-
+  var userQuery = document.getElementById("user-query").value;
+  var userFormat = document.getElementById("format-select").value;
   //Build the path
-
+  var resultsPage =
+    "./search-results.html" + "?q=" + userQuery + "&format=" + userFormat;
+  document.location.replace(resultsPage);
   //Location. replace with^^
-
-
 }
-fetchButton.addEventListener('click', redirectPage);
+fetchButton.addEventListener("click", redirectPage);
