fc7c2138a91717f423e5794c897f7349e1150696
Max Sickels <maxsickels13@gmail.com>
2022-08-18 20:08:22 -0500

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var userContainer = document.getElementById('users');
+var fetchButton = document.getElementById('fetch-button');
+
+
+function redirectPage() {
+  var userQuery = document.getElementById('user-query').value;
+  //Capture User Input
+
+  //Build the path
+
+  //Location. replace with^^
+
+
+}
+fetchButton.addEventListener('click', redirectPage);
